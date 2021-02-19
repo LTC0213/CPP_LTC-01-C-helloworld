8520849f02d7b2cf79c93efc6b87042be804d42c
LTC0213 <ltc970213@gmail.com>
2021-02-19 21:15:38 +0800

    0219

---
+++ 01 C++书写helloworld/01 C++书写HelloWorld.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+/*
+	多行注释
+	main函数
+	每个程序有且只有一个
+*/
+
+#define Day 7
+
+int main()
+{
+
+
+	//整型输入
+	//int a = 0;
+	//cout << "请输入整型变量：" << endl;
+	//cin >> a;
+	//cout << a << endl;
+
+	////浮点型输入
+	//double d = 0;
+	//cout << "请输入浮点型变量：" << endl;
+	//cin >> d;
+	//cout << d << endl;
+
+	////字符型输入
+	//char ch = 0;
+	//cout << "请输入字符型变量：" << endl;
+	//cin >> ch;
+	//cout << ch << endl;
+
+	////字符串型输入
+	//string str;
+	//cout << "请输入字符串型变量：" << endl;
+	//cin >> str;
+	//cout << str << endl;
+
+	////布尔类型输入
+	//bool flag = true;
+	//cout << "请输入布尔型变量：" << endl;
+	//cin >> flag;
+	//cout << flag << endl;
+
+	//char str1[] = "hello world";
+	//cout << str1 << endl;
+
+	//string str = "Hello World";//需要加头文件 #include<string>
+	//cout << str << endl;
+	
+	//char ch = 'a';
+	//cout << ch << endl;
+	//cout << sizeof(char) << endl;
+
+	////ch = "abcde"; //错误，不可以用双引号
+	////ch = 'abcde'; //错误，单引号内只能引用一个字符
+
+	//cout << (int)ch << endl;  //查看字符a对应的ASCII码
+	//ch = 100; //可以直接用ASCII给字符型变量赋值
+	//cout << ch << endl;
+	
+	//float f1 = 3.14f;
+	////float f1 = 3.14;
+	//double d1 = 3.14;
+
+	//cout << f1 << endl;
+	//cout << d1 << endl;
+
+	//cout << "float  sizeof = " << sizeof(f1) << endl;
+	//cout << "double sizeof = " << sizeof(d1) << endl;
+
+	////科学计数法
+	//float f2 = 3e2; // 3 * 10 ^ 2 
+	//cout << "f2 = " << f2 << endl;
+
+	//float f3 = 3e-2;  // 3 * 0.1 ^ 2
+	//cout << "f3 = " << f3 << endl;
+
+
+
+	//cout << "Hello World" << endl;
+	////单行注释 屏幕中输出HelloWorld 
+
+	//cout << "一周总共有 " << Day << " 天" << endl;
+
+	//const int month = 12;//也是常量
+	//
+	//cout << "一年总共有 " << month<< " 个月份" << endl;
+
+	//cout << "int 类型所占内存空间为： " << sizeof(int) << "\nmonth所占内存空间为： " << sizeof(month) << endl;
+
+	system("pause");
+
+	return 0;
+}
